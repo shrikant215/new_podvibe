531d51495b7e4e04a4f648bc124b552c6080208c
shrikant215 <shrikantjk3@gmail.com>
2025-01-25 07:39:35 +0530

    updated passport link

--- podcasts/src/api/index.js
+++ podcasts/src/api/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 // import jwt_decode from 'jwt-decode';
 
-const API = axios.create({ baseURL: `http://localhost:4000/` }); 
+const API = axios.create({ baseURL: `https://podvibe-backend-server.onrender.com/` }); 
 
 //auth
 export const signIn = async ({ email, password }) => await API.post('/auth/login', { email, password });
